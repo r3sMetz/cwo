79a74fd3f3964450e3af799730c497ff2e70443f
Sascha Metz <metz@raum3.de>
2017-04-12 21:46:53 +0200

    Wp Config Env Staging

--- env/wp-config.server.php
+++ env/wp-config.server.php
@@ -6,31 +6,20 @@
  * Ersetze datenbankname_hier_einfuegen
  * mit dem Namen der Datenbank, die du verwenden möchtest.
  */
-define('DB_NAME', '');
+define('DB_NAME', 'cwo_ev_de');
 
 /**
  * Ersetze benutzername_hier_einfuegen
  * mit deinem MySQL-Datenbank-Benutzernamen.
  */
-define('DB_USER', '');
+define('DB_USER', 'cwo_ev_de');
 
 /**
  * Ersetze passwort_hier_einfuegen mit deinem MySQL-Passwort.
  */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'mightymetal');
 
 /**
  * Ersetze localhost mit der MySQL-Serveradresse.
  */
-define('DB_HOST', '');
-
-/**
- * Der Datenbankzeichensatz, der beim Erstellen der
- * Datenbanktabellen verwendet werden soll
- */
-define('DB_CHARSET', 'utf8');
-
-/**
- * Der Collate-Type sollte nicht geändert werden.
- */
-define('DB_COLLATE', '');
+define('DB_HOST', 'cwo-ev.de.mysql');
