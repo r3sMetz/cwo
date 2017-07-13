fcfa5605d8a90155a80f289201cce099c99c637c
Sascha Metz <metz@raum3.de>
2017-07-13 20:33:11 +0200

    new taskrunners

--- wp-content/themes/cwo2017/gulpfile.js
+++ wp-content/themes/cwo2017/gulpfile.js
@@ -5,9 +5,10 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
-var minifyCss = require('gulp-minify-css');
+var cleanCss = require('gulp-clean-css');
 var livereload = require('gulp-livereload');
 var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create('test');
 
 
 var paths = {
@@ -16,21 +17,34 @@ var paths = {
     scripts: ['assets/js/*.js'],
     plugins: [
         'bower_components/jquery/dist/jquery.js',
-        'bower_components/fullpage.js/jquery.fullPage.js'
     ],
-    css : ['bower_components/fullpage.js/jquery.fullPage.css']
+    css : []
 };
 
-gulp.task('sass', function () {
+
+gulp.task('serve',function() {
+
+    browserSync.init({
+        proxy  : 'tutorial',
+        notify : false
+    });
+
+    gulp.watch(paths.scss,['sass']);
+    gulp.watch(paths.scripts, ['compress']);
+    gulp.watch(paths.php).on('change',browserSync.reload);
+    gulp.watch('assets/css/styles.css').on('change',browserSync.reload);
+    gulp.watch('assets/js/build/*.js').on('change',browserSync.reload);
+});
+
+gulp.task('sass',function(){
     gulp.src('assets/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('styles.css'))
         .pipe(autoprefixer({
             browsers: '> 5%'
         }))
-        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(cleanCss({compatibility: 'ie8'}))
         .pipe(gulp.dest('assets/css'))
-        .pipe(livereload())
 });
 
 gulp.task('compress', function () {
@@ -38,8 +52,8 @@ gulp.task('compress', function () {
         .pipe(concat('scripts.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('assets/js/build'))
-        .pipe(livereload())
 });
+
 
 gulp.task('plugins', function(){
     gulp.src(paths.plugins)
@@ -51,15 +65,20 @@ gulp.task('plugins', function(){
         .pipe(autoprefixer({
             browsers: '> 5%'
         }))
-        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(cleanCss({compatibility: 'ie8'}))
         .pipe(gulp.dest('assets/css'));
 });
+
+
+
 
 gulp.task('watch', function () {
     livereload({start: true});
     gulp.watch(paths.scss, ['sass']);
     gulp.watch(paths.scripts, ['compress']);
-    gulp.src(paths.php).pipe(watch(paths.php)).pipe(livereload())
+    gulp.src(paths.php).pipe(watch(paths.php)).pipe(livereload());
+    gulp.src('assets/js/build/*.js').pipe(watch('assets/js/build/*.js')).pipe(livereload());
+    gulp.src('assets/css/*.css').pipe(watch('assets/css/*.css')).pipe(livereload());
 });
 
 gulp.task('default', ['sass', 'compress', 'plugins']);
