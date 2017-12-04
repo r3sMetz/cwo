a70ebd2ea000dca0cabd4e14117489a32a5bac0e
Sascha Metz <metz@raum3.de>
2017-12-04 17:24:58 +0100

    Current Menu Stat

--- wp-content/themes/cwo2017/includes/helper/helper_front-page.php
+++ wp-content/themes/cwo2017/includes/helper/helper_front-page.php
@@ -8,8 +8,14 @@
 			$name_search  = array('ä','ö','ü');
 			$name_replace = array('ae','oe','ue');
 
+			//Reverse String Words in Name
+			$name       = str_replace($name_search,$name_replace,get_the_title(get_sub_field('mitglied')));
+			$name_array = explode(' ',$name);
+			if(count($name_array)>1)
+				$name = $name_array[1].' '.$name_array[0];
+
 			$hq_array[] = array(
-				'name'     => str_replace($name_search,$name_replace,get_the_title(get_sub_field('mitglied'))),
+				'name'     => $name,
 				'position' => get_sub_field('position'),
 				'bild'     => get_sub_field('bild'),
 			);
--- wp-content/themes/cwo2017/modules/navbar.php
+++ wp-content/themes/cwo2017/modules/navbar.php
@@ -1,12 +1,16 @@
-<div class="cwo_navbar puffer-both-md">
+<div class="cwo_navbar">
 	<div class="fullPageWrapper">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-6">
+				<div class="col-sm-6 text-center-xs text-left-sm">
 					<img src="<?php echo CWO_PNG_URI;?>/logo_w_font.png" alt="Celtic Warriors Otzenhausen">
 				</div>
-				<div class="col-xs-6 text-right">
-					Menu
+				<div class="col-sm-6 text-center-xs text-right-sm">
+					<ul class="list-inline">
+                        <?php foreach(r3_getMenue('HauptmenuDE') as $item):?>
+                            <li><a class="fadeLink whiteLink" href="<?php echo $item->url;?>"><?php echo $item->title;?></a></li>
+                        <?php endforeach;?>
+                    </ul>
 				</div>
 			</div>
 		</div>
