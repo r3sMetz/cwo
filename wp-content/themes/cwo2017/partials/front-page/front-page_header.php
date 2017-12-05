82751f749037c61e22dc0cb257b762471d1013e1
Sascha Metz <metz@raum3.de>
2017-12-05 21:50:29 +0100

    Header Hero Death4

--- wp-content/themes/cwo2017/partials/front-page/front-page_header.php
+++ wp-content/themes/cwo2017/partials/front-page/front-page_header.php
@@ -4,44 +4,10 @@
             <div class="container">
                 <div class="row front-page_header_content_wrapper">
                     <!-- Headline -->
-                    <div class="col-sm-4 col-md-5 text-center front-page_header_headline">
+                    <div class="col-sm-12 text-center front-page_header_headline">
                         <h1>
                             <span class="font-lord da_head pos-rel dp-inline-block"><?php the_field('headline');?></span><br/>
                             <span><?php the_field('subline');?></span></h1>
-                    </div>
-                    <!-- Images -->
-                    <div class="col-sm-4 col-md-3 hidden-xs front-page_header_images pos-rel">
-                        <img class="maxImage" src="<?php echo get_field('headervorschau_oben')['sizes']['medium_large'];?>" alt="CWO">
-                        <img class="maxImage" src="<?php echo get_field('headervorschau_unten')['sizes']['medium_large'];?>" alt="CWO">
-                    </div>
-                    <!-- Next Event (Desktop)  -->
-                    <div class="col-sm-4 col-md-4 hidden-xs">
-                        <div class="front-page_header_next_event desktop bg-pommes text-center puffer-both-sm">
-                            <p class="font-demon fontsize-headline gap-bottom-md">Samstag 18.02.2017</p>
-                            <h2 class="font-demon fontsize-headline-big color-black gap-bottom-sm">Steel Crusade 31</h2>
-                            <p class="gap-bottom-sm">
-                                Die Celtic Warriors Otzenhausen<br/>
-                                präsentieren das 31. Steelcursade:
-                            </p>
-
-                            <ul class="list-unstyled color-black font-demon text-uppercase list-dashed gap-bottom-md">
-                                <li>Revolution 9</li>
-                                <li>Desdemonia</li>
-                                <li>Fusion Bomb</li>
-                            </ul>
-
-                            <ul class="list-unstyled gap-bottom-md">
-                                <li>Einlass 19:30</li>
-                                <li>Beginn: 20:00Uhr</li>
-                                <li>Eintritt: 5Euro</li>
-                            </ul>
-
-                            <p>
-                                Jugendraum<br/>
-                                Ringwalstraße 8<br/>
-                                66620 Otzenhausen
-                            </p>
-                        </div>
                     </div>
                 </div>
             </div>
