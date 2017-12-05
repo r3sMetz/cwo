1870127a5eab6944ac0bd515dbf95a35b2eb8826
Sascha Metz <metz@raum3.de>
2017-12-05 21:22:08 +0100

    Fixed Navigation Problem

--- wp-content/themes/cwo2017/partials/front-page/front-page_header.php
+++ wp-content/themes/cwo2017/partials/front-page/front-page_header.php
@@ -10,16 +10,16 @@
                             <span><?php the_field('subline');?></span></h1>
                     </div>
                     <!-- Images -->
-                    <div class="col-sm-4 col-md-3 hidden-xs front-page_header_images">
+                    <div class="col-sm-4 col-md-3 hidden-xs front-page_header_images pos-rel">
                         <img class="maxImage" src="<?php echo get_field('headervorschau_oben')['sizes']['medium_large'];?>" alt="CWO">
                         <img class="maxImage" src="<?php echo get_field('headervorschau_unten')['sizes']['medium_large'];?>" alt="CWO">
                     </div>
                     <!-- Next Event (Desktop)  -->
                     <div class="col-sm-4 col-md-4 hidden-xs">
-                        <div class="front-page_header_next_event desktop bg-pommes text-center puffer-both-xl">
+                        <div class="front-page_header_next_event desktop bg-pommes text-center puffer-both-sm">
                             <p class="font-demon fontsize-headline gap-bottom-md">Samstag 18.02.2017</p>
-                            <h2 class="font-demon fontsize-headline-big color-black gap-bottom-md">Steel Crusade 31</h2>
-                            <p class="gap-bottom-md">
+                            <h2 class="font-demon fontsize-headline-big color-black gap-bottom-sm">Steel Crusade 31</h2>
+                            <p class="gap-bottom-sm">
                                 Die Celtic Warriors Otzenhausen<br/>
                                 präsentieren das 31. Steelcursade:
                             </p>
