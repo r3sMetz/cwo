7c93c1f0aaad78d4d54d6004151cecd9d7d99af6
Sascha Metz <metz@raum3.de>
2017-12-29 15:50:05 +0100

    Little Horst Corrections

--- wp-content/themes/cwo2017/partials/page-memorial/page-memorial_content.php
+++ wp-content/themes/cwo2017/partials/page-memorial/page-memorial_content.php
@@ -4,7 +4,7 @@
 			<div class="col-md-4 text-center gap-mobile-bottom-md">
 				<img src="<?php echo CWO_JPG_URI;?>/horst_1.jpg" alt="RIP Horst!" class="maxImage">
 			</div>
-			<div class="col-md-8">
+			<div class="col-md-8 iframe_wrapper">
 				<iframe src="https://open.spotify.com/embed?uri=spotify:user:metz1988:playlist:4nyT7f1FNpO3kOuNlMUwZr" frameborder="0" allowtransparency="true"></iframe>
 			</div>
 		</div>
