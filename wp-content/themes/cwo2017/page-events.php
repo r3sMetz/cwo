795e18d450ee28c730c89f3213701f463b1ddeeb
Sascha Metz <metz@raum3.de>
2017-12-29 15:39:24 +0100

    Initial Events Page

---
+++ wp-content/themes/cwo2017/page-events.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Template Name: Events
+ */
+
+/** HTML Begin **/
+get_template_part( 'modules/html_begin' );
+
+/*** Navbar ***/
+get_template_part( 'modules/navbar' );
+
+/** HTML End **/
+get_template_part( 'modules/html_end' );
