2f2c0c1125bbd37d8c46009d825d20862a7cf53e
Sascha Metz <metz@raum3.de>
2018-01-23 22:41:16 +0100

    Memorial with Dashed Line

--- wp-content/themes/cwo2017/partials/page-memorial/page-memorial_content.php
+++ wp-content/themes/cwo2017/partials/page-memorial/page-memorial_content.php
@@ -1,5 +1,12 @@
 <div class="page-memorial_content fullPageElement flexed_center puffer-both-xl margin-top-mobile bg-std bg-clouds">
 	<div class="container">
+		<div class="row gap-bottom-xl">
+            <div class="col-md-12 text-center">
+                <h1 class="font-demon fontsize-headline-big dashed-headline">
+                    <span class="dashed_line"></span><span class="dashed_text"><?php the_title();?></span><span class="dashed_line"></span>
+                </h1>
+            </div>
+        </div>
 		<div class="row">
 			<div class="col-md-4 text-center gap-mobile-bottom-md pos-rel">
                 <img id="the_cap" src="<?php echo CWO_SVG_URI;?>/cap.svg" alt="Heavy Metal - Watt eh Scheiß!!!" class="hidden-xs hidden-sm">
