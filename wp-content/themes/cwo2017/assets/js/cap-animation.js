cf8b45c86123b83ff35a8e02eefb829db8366122
Sascha Metz <metz@raum3.de>
2018-01-23 21:33:33 +0100

    Horsti

--- wp-content/themes/cwo2017/assets/js/cap-animation.js
+++ wp-content/themes/cwo2017/assets/js/cap-animation.js
@@ -1,14 +1,36 @@
 var capAnimation = (function(){
+
     /** Public **/
     function setup(){
-        //Set Options
-        var the_cap = $('#the_cap');
+        if(!device.isMobile && device.width() >= device.screen.md) {
+            //Set Cap Jquery Object
+            var the_cap = $('#the_cap');
+
+            //Show that cap has Interaction
+            the_cap.addClass('playable');
+
+            //Init Laugh Array
+            var all_laughs = Array(4);
+
+            //Load All Laughs
+            $.each(all_laughs, function (index) {
+                all_laughs[index] = new Audio(defaults.template_directory_uri + '/assets/wav/lache_' + index + '.wav')
+            });
 
 
-        //Set Intervall
-        setInterval(function(){
-            the_cap.toggleClass('moving');
-        },2500)
+            //Set Intervall for Cap Animation
+            setInterval(function () {
+                the_cap.toggleClass('moving');
+            }, 2500);
+
+
+            //Set Click Event for the Cap
+            the_cap.on('click', function () {
+                var the_laugh = all_laughs[Math.floor(Math.random() * all_laughs.length)];
+
+                the_laugh.play();
+            })
+        }
     }
 
     return {
