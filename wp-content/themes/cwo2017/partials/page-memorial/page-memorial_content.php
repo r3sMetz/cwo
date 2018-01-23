d75037e21b07c6e15b51d968a1d7f070f8db5f34
Sascha Metz <metz@raum3.de>
2018-01-23 20:15:42 +0100

    Added Horst Layout

--- wp-content/themes/cwo2017/partials/page-memorial/page-memorial_content.php
+++ wp-content/themes/cwo2017/partials/page-memorial/page-memorial_content.php
@@ -1,8 +1,8 @@
-<div class="page-memorial_content fullPageElement flexed_center puffer-both-xl margin-top-mobile">
+<div class="page-memorial_content fullPageElement flexed_center puffer-both-xl margin-top-mobile bg-std bg-clouds">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4 text-center gap-mobile-bottom-md">
-				<img src="<?php echo CWO_JPG_URI;?>/horst_1.jpg" alt="RIP Horst!" class="maxImage">
+				<img src="<?php echo CWO_PNG_URI;?>/horst.png" alt="RIP Horst!" class="maxImage">
 			</div>
 			<div class="col-md-8 iframe_wrapper">
 				<iframe src="https://open.spotify.com/embed?uri=spotify:user:metz1988:playlist:4nyT7f1FNpO3kOuNlMUwZr" frameborder="0" allowtransparency="true"></iframe>
