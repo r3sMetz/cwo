1c1d7f57f688317428f7ceb581e17c954500d6a7
Sascha Metz <metz@raum3.de>
2018-01-24 13:34:21 +0100

    events pt.1

---
+++ wp-content/themes/cwo2017/modules/cwo_eventheader.php
@@ -0,0 +1,9 @@
+<header class="bg-std cwo_eventheader bg-dude mobile-bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="font-demon fontsize-headline-big"><?php the_title();?></h1>
+			</div>
+		</div>
+	</div>
+</header>
--- wp-content/themes/cwo2017/page-events.php
+++ wp-content/themes/cwo2017/page-events.php
@@ -9,5 +9,11 @@ get_template_part( 'modules/html_begin' );
 /*** Navbar ***/
 get_template_part( 'modules/navbar' );
 
+/*** Eventheader ***/
+get_template_part('modules/cwo_eventheader');
+
+/*** Footer ***/
+get_template_part('modules/cwo_footer');
+
 /** HTML End **/
 get_template_part( 'modules/html_end' );
