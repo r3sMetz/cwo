a3c49be9c8bbe2b6a08aa77ce7568c443a86cc93
Sascha Metz <metz@raum3.de>
2018-01-28 22:15:50 +0100

    FrontPage Update

--- wp-content/themes/cwo2017/partials/page-bilder/page-bilder_content.php
+++ wp-content/themes/cwo2017/partials/page-bilder/page-bilder_content.php
@@ -4,12 +4,14 @@
 
 		    <!-- Gallery Thumbs -->
             <div class="galery_wrapper puffer-both-xl">
+            <?php for($i=0;$i<=10;$i++):?>
             <?php foreach(cwo_getGaleries() as $galery):?>
             <a href="<?php echo $galery['url'];?>" class="single_galery text-center fadeLink">
                 <img src="<?php echo $galery['thumb'];?>" alt="<?php echo $galery['title'];?>">
                 <h3 class="font-demon fontsize-headline gap-top-sm"><?php echo $galery['title'];?></h3>
             </a>
             <?php endforeach;?>
+            <?php endfor;?>
             </div>
         </div>
 	</div>
