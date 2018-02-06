bc8dcd66982131566c1ef5d08ed9c1ce97c78dba
Sascha Metz <metz@raum3.de>
2018-02-06 23:23:07 +0100

    Normalized Gallery2

--- wp-content/themes/cwo2017/partials/single-galerien/single-galerien_content.php
+++ wp-content/themes/cwo2017/partials/single-galerien/single-galerien_content.php
@@ -1,5 +1,10 @@
 <div class="single-galerien_content fullPageElement flexed_center puffer-both-xxl margin-top-mobile" id="the_gallery_wrapper">
 	<div class="container">
+	    <div class="row gap-bottom-sm">
+	        <div class="col-md-12">
+                <h1 class="font-demon fontsize-headline-big"><?php the_title();?></h1>
+            </div>
+        </div>
 		<!-- Gallery Overview -->
 		<div class="row gap-bottom-md">
 			<div class="col-md-12">
