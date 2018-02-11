81dedc7b5be48a80bcec6886c09dbaf8bb003598
Sascha Metz <metz@raum3.de>
2018-02-11 22:46:43 +0100

    Current State

--- wp-content/themes/cwo2017/partials/page-events/page-events_content.php
+++ wp-content/themes/cwo2017/partials/page-events/page-events_content.php
@@ -6,8 +6,8 @@
 					<span><?php echo $event->data_array[0];?></span><br/>
 					<span><?php echo $event->data_array[1];?></span>
 				</div>
-				<div class="col-sm-3">Titel</div>
-				<div class="col-sm-4">Paragraph</div>
+				<div class="col-sm-3"><?php echo get_the_title($event->ID);?></div>
+				<div class="col-sm-4"><?php the_field('infoblock_2',$event->ID);?></div>
 				<div class="col-sm-2">Uhrzeit</div>
 				<div class="col-sm-2 text-right-sm">Button</div>
 			</div>
