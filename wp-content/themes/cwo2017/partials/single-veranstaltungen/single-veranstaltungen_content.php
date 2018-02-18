1b37e1459cec5d82e7b490002c3ccdf1843a7382
Sascha Metz <metz@raum3.de>
2018-02-18 22:36:07 +0100

    Current State

--- wp-content/themes/cwo2017/partials/single-veranstaltungen/single-veranstaltungen_content.php
+++ wp-content/themes/cwo2017/partials/single-veranstaltungen/single-veranstaltungen_content.php
@@ -1,22 +1,79 @@
 <div class="single-veranstaltungen_content puffer-both-xl">
 	<div class="container">
-		<!-- Top Line -->
+		<!-- Bandlogos-->
+		<?php if(get_field('alle_bandlogos')):?>
 		<div class="row">
-			<div class="col-md-12">
-				<hr>
-			</div>
-		</div>
+            <div class="col-md-4">Logo 1</div>
+            <div class="col-md-4">Logo 2</div>
+            <div class="col-md-4">Logo 3</div>
+        </div
+        <?php endif;?>
+		<!-- Texting Top -->
+		<div class="row">
+            <div class="col-sm-6">
+                <h2 class="font-demon fontsize-headline gap-bottom-sm">Info</h2>
+                <p>
+                Bacon ipsum dolor amet venison sausage short loin turducken,
+                tongue sirloin pork belly meatloaf tri-tip. Beef ribs porchetta ball tip,
+                </p>
+            </div>
+            <div class="col-sm-6">
+                <h2 class="font-demon fontsize-headline gap-bottom-sm">Wo?</h2>
+                <p>
+                Hunnenringhalle<br/>
+                Ringwalstraße 1<br/>
+                66620 Otzenhausen<br/>
+                </p>
+            </div>
+        </div>
 
-		<div class="row">
-			<div class="col-md-6"></div>
-			<div class="col-md-6"></div>
-		</div>
+
+		<!-- Top Line -->
+        <div class="row top-line">
+            <div class="col-md-12">
+                <hr>
+            </div>
+        </div>
+
+        <!-- Texting WYSIWYG -->
+        <div class="row">
+            <div class="col-md-9">
+                <h2 class="font-demon fontsize-headline gap-bottom-sm gap-top-md">Band 1</h2>
+                <p>
+                Bacon ipsum dolor amet venison sausage short loin turducken, tongue sirloin pork belly meatloaf tri-tip. Beef ribs porchetta ball tip, strip
+                steak picanha frankfurter sirloin jowl. Rump boudin fatback, shank ground round sausage swine cupim leberkas brisket alcatra ball tip turkey
+                hamburger prosciutto. Short loin pork kevin, brisket leberkas ball tip ground round jerky frankfurter doner salami corned beef kielbasa
+                burgdoggen pork belly. Ham hock alcatra pork loin, shoulder spare ribs t-bone capicola bresaola prosciutto ham strip steak rump cow shankle
+                porchetta.
+                </p>
+
+                <h2 class="font-demon fontsize-headline gap-bottom-sm gap-top-md">Band 2</h2>
+                <p>
+                Bacon ipsum dolor amet venison sausage short loin turducken, tongue sirloin pork belly meatloaf tri-tip. Beef ribs porchetta ball tip, strip
+                steak picanha frankfurter sirloin jowl. Rump boudin fatback, shank ground round sausage swine cupim leberkas brisket alcatra ball tip turkey
+                hamburger prosciutto. Short loin pork kevin, brisket leberkas ball tip ground round jerky frankfurter doner salami corned beef kielbasa
+                burgdoggen pork belly. Ham hock alcatra pork loin, shoulder spare ribs t-bone capicola bresaola prosciutto ham strip steak rump cow shankle
+                porchetta.
+                </p>
+
+                <h2 class="font-demon fontsize-headline gap-bottom-sm gap-top-md">Band 2</h2>
+                <p>
+                Bacon ipsum dolor amet venison sausage short loin turducken, tongue sirloin pork belly meatloaf tri-tip. Beef ribs porchetta ball tip, strip
+                steak picanha frankfurter sirloin jowl. Rump boudin fatback, shank ground round sausage swine cupim leberkas brisket alcatra ball tip turkey
+                hamburger prosciutto. Short loin pork kevin, brisket leberkas ball tip ground round jerky frankfurter doner salami corned beef kielbasa
+                burgdoggen pork belly. Ham hock alcatra pork loin, shoulder spare ribs t-bone capicola bresaola prosciutto ham strip steak rump cow shankle
+                porchetta.
+                </p>
+            </div>
+        </div>
+
+
 
 		<!-- Bottom Line -->
-		<div class="row">
-			<div class="col-md-12">
-				<hr>
-			</div>
-		</div>
+        <div class="row">
+            <div class="col-md-12">
+                <hr>
+            </div>
+        </div>
 	</div>
 </div>
