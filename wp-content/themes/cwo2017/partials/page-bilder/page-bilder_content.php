dfd8e78e9234b91cf1deb61289ae951b7fb42421
Sascha Metz <metz@raum3.de>
2018-03-20 22:50:04 +0100

    Page Bilder Update

--- wp-content/themes/cwo2017/partials/page-bilder/page-bilder_content.php
+++ wp-content/themes/cwo2017/partials/page-bilder/page-bilder_content.php
@@ -1,20 +1,22 @@
 <div class="fullPageWrapper">
 	<div class="page-bilder_content bg-std flexed_center">
         <div class="container">
-            <div class="col-md-12 the_galleries">
-                <?//TODO: METZ: Remove Index before Launch?>
-                <?for($index=0;$index<3;$index++):?>
-                <?foreach(cwo_getGaleries() as $key => $galery):?>
-                    <div class="single_gallery" data-url="<?=$galery['url'];?>">
-                        <div class="single_gallery_inner">
-                            <div class="single_gallery_text">
-                                <h3 class="font-heat fontsize-bigger"><?=$galery['title'];?></h3>
-                                <img class="hidden-xs" src="<?=CWO_PNG_URI;?>/eye_icon.png" alt="CWO Show Gallery">
+            <div class="row">
+                <div class="col-md-12 the_galleries">
+                    <?//TODO: METZ: Remove Index before Launch?>
+                    <?for($index=0;$index<3;$index++):?>
+                    <?foreach(cwo_getGaleries() as $key => $galery):?>
+                        <div class="single_gallery" data-url="<?=$galery['url'];?>">
+                            <div class="single_gallery_inner">
+                                <div class="single_gallery_text">
+                                    <h3 class="font-heat fontsize-bigger"><?=$galery['title'];?></h3>
+                                    <img class="hidden-xs" src="<?=CWO_PNG_URI;?>/eye_icon.png" alt="CWO Show Gallery">
+                                </div>
                             </div>
                         </div>
-                    </div>
-                <?endforeach;?>
-                <?endfor;?>
+                    <?endforeach;?>
+                    <?endfor;?>
+                </div>
             </div>
         </div>
 	</div>
