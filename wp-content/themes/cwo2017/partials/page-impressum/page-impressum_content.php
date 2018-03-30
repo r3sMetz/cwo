0040003cd3ac8ecf5086cd13d809ceea2ec4c85e
Sascha Metz <metz@raum3.de>
2018-03-30 23:01:48 +0200

    Spacing Update

--- wp-content/themes/cwo2017/partials/page-impressum/page-impressum_content.php
+++ wp-content/themes/cwo2017/partials/page-impressum/page-impressum_content.php
@@ -1,10 +1,10 @@
 <div class="page-impressum_content puffer-both-xl">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col-sm-4">
                 <?the_knauped_imprint(get_field('linke_seite'));?>
 			</div>
-			<div class="col-md-8 margin-top-mobile">
+			<div class="col-sm-8 margin-top-mobile">
                 <?the_knauped_imprint(get_field('rechte_seite'));?>
             </div>
 		</div>
