7473d9c15551327b6c5610c461b0dbe2e50b74c4
Sascha Metz <metz@raum3.de>
2018-03-30 22:26:56 +0200

    Added Imprint Link

--- wp-content/themes/cwo2017/modules/cwo_footer.php
+++ wp-content/themes/cwo2017/modules/cwo_footer.php
@@ -4,18 +4,18 @@
 			<div class="row cwo_footer_content">
 				<div class="col-md-3">
 					<ul class="list-inline dashed-top-left half-width font-heat fontsize-bigger">
-                        <li><a class="fadeLink whiteLink" href="#">Impressum</a></li>
+                        <li><a class="fadeLink whiteLink" href="<?=get_permalink(342);?>">Impressum</a></li>
 					</ul>
 				</div>
 				<div class="col-md-3">
 				    <?if(get_the_ID() === 282):?>
 				        <span class="cwo-btn cwo-btn-red cwo_loc_back">Abbrechen</span>
 				    <?else:?>
-					    <a href="<?php echo get_permalink(282);?>" class="cwo-btn cwo-btn-red fadeLink">Newsletter abonnieren</a>
+					    <a href="<?=get_permalink(282);?>" class="cwo-btn cwo-btn-red fadeLink">Newsletter abonnieren</a>
 					<?php endif;?>
 				</div>
 				<div class="col-md-3">
-                    <img class="maxImage" src="<?php echo CWO_PNG_URI;?>/against_racism.png" alt="CWO - Against Racism">
+                    <img class="maxImage" src="<?=CWO_PNG_URI;?>/against_racism.png" alt="CWO - Against Racism">
                 </div>
 			</div>
 			<div class="row gap-top-sm">
