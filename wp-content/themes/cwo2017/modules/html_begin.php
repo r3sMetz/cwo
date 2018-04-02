30fe65ae720cb417d02843638d846c83b9e89e93
Sascha Metz <metz@raum3.de>
2018-04-02 22:47:59 +0200

    no title tag

--- wp-content/themes/cwo2017/modules/html_begin.php
+++ wp-content/themes/cwo2017/modules/html_begin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?bloginfo('name');?></title>
+    <!-- <title><?bloginfo('name');?></title>-->
     <? wp_head();?>
 </head>
 <body class="bg-black">
