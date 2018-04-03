841a89a23fa66384cc00f0deb439d222059036e1
Sascha Metz <metz@raum3.de>
2018-04-03 20:07:12 +0200

    Double Qoutes

--- wp-content/themes/cwo2017/includes/ajax/ajax_contact.php
+++ wp-content/themes/cwo2017/includes/ajax/ajax_contact.php
@@ -5,8 +5,8 @@ function contact_ajax() {
 		$subject = "Cwo Website - Neue Kontaktanfrage";
 		$headers = [];
 		$message = $_REQUEST['message'];
-		$message.= '\n\nGesendet von: '.$_REQUEST["name"];
-		$message.= '\n\nEmail: '.$_REQUEST["email"];
+		$message.= "\n\nGesendet von: ".$_REQUEST["name"];
+		$message.= "\n\nEmail: ".$_REQUEST["email"];
 
 		if(wp_mail($to,$subject,$message,$headers) === true){
 			http_response_code(200);
