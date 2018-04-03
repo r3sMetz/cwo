3a404821587dcfad56de2d2d030257aefa49a19b
Sascha Metz <metz@raum3.de>
2018-04-03 20:20:45 +0200

    From Reply to3

--- wp-content/themes/cwo2017/includes/ajax/ajax_contact.php
+++ wp-content/themes/cwo2017/includes/ajax/ajax_contact.php
@@ -3,9 +3,10 @@ function contact_ajax() {
 	if ( isset( $_REQUEST ) ) {
 		$to = CWO_MAILING_ADRESS;
 		$subject = "Cwo Website - Neue Kontaktanfrage";
-		$headers = 'From: '.$_REQUEST["name"]."\r\n";
-		$headers.= 'Reply-To: '.$_REQUEST["email"];
-		$message = $_REQUEST["message"];
+		$headers = [];
+		$message = $_REQUEST['message'];
+		$message.= "\n\nGesendet von: ".$_REQUEST["name"];
+		$message.= "\n\nEmail: ".$_REQUEST["email"];
 
 		if(wp_mail($to,$subject,$message,$headers) === true){
 			http_response_code(200);
