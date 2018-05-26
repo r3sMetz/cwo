91d2d065e234a0b94f4247a69e3f6e4077541d91
Sascha Metz <metz@raum3.de>
2018-05-26 12:16:41 +0200

    Decent 404 -2

--- wp-content/themes/cwo2017/partials/404/404-content.php
+++ wp-content/themes/cwo2017/partials/404/404-content.php
@@ -1,8 +1,8 @@
 <div class="container" style="padding: 100px 0;">
 	<div class="row">
-		<div class="col-12 text-center">
+		<div class="col-md-12 text-center">
 			<h1 class="font-heat fontsize-headline">Seite nicht gefunden</h1><br>
-			<img src="<?=get_template_directory_uri();?>/assets/img/404.gif" alt="CWO - 404"/>
+			<img style="max-width: 100%;" src="<?=get_template_directory_uri();?>/assets/img/404.gif" alt="CWO - 404"/>
 		</div>
 	</div>
 </div>
