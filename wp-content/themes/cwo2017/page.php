b4fc384de525a9710145bf4bc7ed83b94502b276
Sascha Metz <metz@raum3.de>
2018-07-10 22:17:36 +0200

    Added Page php

---
+++ wp-content/themes/cwo2017/page.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Template Name: Impressum
+ */
+
+/** HTML Begin **/
+get_template_part( 'modules/html_begin' );
+
+/*** Navbar ***/
+get_template_part( 'modules/navbar' );
+
+/*** Eventheader ***/
+get_template_part('modules/cwo_eventheader');
+
+/*** Content ***/
+;?>
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			<?php while(have_posts()):the_post();?>
+				<?php the_content();?>
+			<?php endwhile;?>
+		</div>
+	</div>
+</div>
+<?php
+/*** Footer ***/
+get_template_part('modules/cwo_footer');
+
+/** HTML End **/
+get_template_part( 'modules/html_end' );
