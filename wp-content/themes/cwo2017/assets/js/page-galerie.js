9693e256c4d67760c46fd8385373df4ad022c29d
Sascha Metz <metz@raum3.de>
2018-10-06 16:07:46 +0200

    Added Show Functionality

---
+++ wp-content/themes/cwo2017/assets/js/page-galerie.js
@@ -0,0 +1,52 @@
+var pageGalerie = (function(){
+	/** Private **/
+	var imageWrapper, imageContainer, loadingImage;
+
+	function openImage(event){
+		// Show Image Container
+		imageWrapper.css('display','flex');
+		setTimeout(function(){
+			imageWrapper.addClass('active');
+		},0);
+
+		// Load Image
+		var newImage = document.createElement('img');
+		newImage.src = $(event.target).data('source');
+
+		// Append new Image onload, hide loading, show image
+		newImage.onload = function(){
+			loadingImage.css('display','none');
+			imageContainer.append(newImage);
+			imageContainer.css('display','block');
+		}
+	}
+
+	function closeImage(){
+		// Hide Image Container
+		imageWrapper.removeClass('active');
+
+		// D-None image-wrapper, clean image container, hide image-container, show loading
+		setTimeout(function(){
+			imageWrapper.css('display','none');
+			imageContainer.css('display','none');
+			imageContainer.html('');
+			imageContainer.css('display','none');
+			loadingImage.css('display','block');
+		},500)
+	}
+
+	/** Public **/
+	function setup(){
+		// Setup Initial Vars
+		imageWrapper = $('.page-galerie_content_detail');
+		imageContainer = $('.page-galerie_content_detail .showImage');
+		loadingImage   = $('.page-galerie_content_detail .loading');
+
+		$('.page-galerie_content img').on('click',openImage);
+		$('.page-galerie_content_detail .close').on('click',closeImage);
+	}
+
+	return {
+		setup:setup
+	}
+})();
--- wp-content/themes/cwo2017/partials/page-galerie/page-galerie_content.php
+++ wp-content/themes/cwo2017/partials/page-galerie/page-galerie_content.php
@@ -4,3 +4,13 @@
 		<img data-source="<?=$image['url'];?>" src="<?=$image['sizes']['medium_large'];?>" alt="<?=$image['name'];?>">
 	<?endforeach;?>
 </main>
+
+<aside class="page-galerie_content_detail">
+    <span class="close cwo-btn cwo-btn-red">Schliessen</span>
+    <div class="loading">
+        <img src="<?=get_theme_file_uri('/assets/img/svg/cap.svg');?>" alt="CWO - Horst Cap">
+    </div>
+    <div class="showImage" style="display: none;">
+
+    </div>
+</aside>
